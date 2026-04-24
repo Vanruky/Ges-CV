2e9a6140cbf4e730c6d08a86e0bdee09905da086
javinovoae <ja.novoae@duocuc.cl>
2026-04-23 21:47:13 -0400

    modificacion bloqueos despues de 1 vez

--- backend/src/models/usuarioModel.js
+++ backend/src/models/usuarioModel.js
@@ -60,32 +60,38 @@ const Candidato = {
         return rows[0];
     },
 
-    // para actualizar la informacion
+    // para actualizar la informacion , el rut,nombre y apellidos solo se edita 1 vez
     actualizarPerfil: async (id_usuario, datos) => {
-        const sql = `
-            UPDATE candidato 
-            SET tipo_identificacion = ?, 
-                numero_identificacion = ?, 
-                nombre = ?, 
-                apellido_paterno = ?, 
-                apellido_materno = ?, 
-                celular = ?,
-                rut_editado = ?
-            WHERE id_usuario = ?
-        `;
-        const valores = [
-            datos.tipo_identificacion, 
-            datos.numero_identificacion,
-            datos.nombre,
-            datos.apellido_paterno,
-            datos.apellido_materno,
-            datos.celular,
-            datos.rut_editado,
-            id_usuario
-        ];
-        const [result] = await db.query(sql, valores);
-        return result.affectedRows > 0; 
-    },
+
+        const [rows] = await db.query(
+        'SELECT rut_editado, nombre_apellidos_editados FROM candidato WHERE id_usuario = ?',
+        [id_usuario]
+        );
+
+    if (rows.length === 0) return false;
+
+    const { rut_editado, nombre_apellidos_editados } = rows[0];
+
+    let campos = ['celular = ?'];
+    let valores = [datos.celular];
+
+    if (rut_editado === 0) {
+        campos.push('tipo_identificacion = ?', 'numero_identificacion = ?', 'rut_editado = 1');
+        valores.push(datos.tipo_identificacion, datos.numero_identificacion);
+    }
+
+    if (nombre_apellidos_editados === 0) {
+        campos.push('nombre = ?', 'apellido_paterno = ?', 'apellido_materno = ?', 'nombre_apellidos_editados = 1');
+        valores.push(datos.nombre, datos.apellido_paterno, datos.apellido_materno);
+    }
+
+    const sql = `UPDATE candidato SET ${campos.join(', ')} WHERE id_usuario = ?`;
+    valores.push(id_usuario);
+
+    const [result] = await db.query(sql, valores);
+    return result.affectedRows > 0;
+},
+
 
     actualizarContrasenia: async (id_usuario, nuevoPasswordHash) => {
     const sql = `UPDATE usuario SET password_hash = ? WHERE id_usuario = ?`;
